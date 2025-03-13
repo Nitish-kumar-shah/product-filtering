48c8d4124893135611baeaf082b707a735f9d5c4
Nitish kumar shah <nitish.zyn@gmail.com>
2025-03-13 23:06:17 +0530

    sidebar componenet complete

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -1,1 +1,23 @@
+import "./Sidebar.css";
+import { IoCartSharp } from "react-icons/io5";
+import Category from "./Category/Category";
+import Price from "./Price/Price";
+import Color from "./Color/Color";
 
+function Sidebar() {
+  return (
+    <>
+      <section className="sidebar">
+        <div className="logo-container">
+          <IoCartSharp className="cart-icon" />
+        </div>
+
+        <Category />
+        <Price />
+        <Color />
+      </section>
+    </>
+  );
+}
+
+export default Sidebar;
