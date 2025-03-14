4929372c0331bba8a6d84274a6556d3e80ccb425
Nitish kumar shah <nitish.zyn@gmail.com>
2025-03-14 09:10:36 +0530

    complere color and price components

--- src/Sidebar/Color/Color.js
+++ src/Sidebar/Color/Color.js
@@ -1,7 +1,37 @@
 import "./Color.css";
 
 function Color() {
-  return <div>Color</div>;
+  return (
+    <div className="color-container">
+      <h2 className="sidebar-title price-title">Price</h2>
+
+      <label className="sidebar-label-container">
+        <input type="radio" name="test2" />
+        <span className="checkmark"></span>All
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test2" />
+        <span className="checkmark"></span>Black
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test2" />
+        <span className="checkmark"></span>Blue
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test2" />
+        <span className="checkmark"></span>Red
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test2" />
+        <span className="checkmark"></span>Green
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test2" />
+        <span className="checkmark"></span>
+        White
+      </label>
+    </div>
+  );
 }
 
 export default Color;
